5c2cb6114185129cfb224a1f20cc1ed64552111f
oblachka81-cloud <oblachka81@gmail.com>
2026-03-25 23:02:19 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Telegraf } = require('telegraf');
+
+const BOT_TOKEN = '8689023992:AAF_xjJkg0MUw3zmitwvRh85RuvBdeeA0Kc';
+const bot = new Telegraf(BOT_TOKEN);
+const app = express();
+
+app.use(express.json());
+
+app.post(`/webhook/${BOT_TOKEN}`, (req, res) => {
+  bot.handleUpdate(req.body, res);
+  res.sendStatus(200);
+});
+
+bot.start((ctx) => {
+  ctx.reply('🧠 Добро пожаловать в NEURON! Игра, где твой ум приносит токены. Скоро запуск!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Бот запущен на порту ${PORT}`);
+});
